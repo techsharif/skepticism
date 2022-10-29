8913aaa2d9f66fdb144cbe7fb29d61b8ae021771
techsharif <sharif.cse.hstu@gmail.com>
2022-10-29 21:05:14 +0600

    add react support

--- src/my-command.js
+++ src/my-command.js
@@ -1,11 +1,77 @@
-import BrowserWindow from 'sketch-module-web-view'
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import {Artboard, render, Text, View} from 'react-sketchapp';
+import chroma from 'chroma-js';
 
-export default function () {
-  const options = {
-    identifier: 'unique.id',
-  }
+// take a hex and give us a nice text color to put over it
+const textColor = (hex) => {
+    const vsWhite = chroma.contrast(hex, 'white');
+    if (vsWhite > 4) {
+        return '#FFF';
+    }
+    return chroma(hex).darken(3).hex();
+};
 
-  const browserWindow = new BrowserWindow(options)
+const Swatch = ({name, hex}) => (
+    <View
+        name={`Swatch ${name}`}
+        style={{
+            height: 96,
+            width: 96,
+            margin: 4,
+            backgroundColor: hex,
+            padding: 8,
+        }}
+    >
+        <Text
+            name="Swatch Name"
+            style={{color: textColor(hex), fontWeight: 'bold', fontFamily: 'Helvetica'}}
+        >
+            {name}
+        </Text>
+        <Text name="Swatch Hex" style={{color: textColor(hex)}}>
+            {hex}
+        </Text>
+    </View>
+);
 
-  browserWindow.loadURL(require('./my-screen.html'))
-}
+const Color = {
+    hex: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+};
+
+Swatch.propTypes = Color;
+
+const Document = ({colors}) => (
+    <Artboard
+        name="Swatches"
+        style={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            width: (96 + 8) * 4,
+        }}
+    >
+        {Object.keys(colors).map((color) => (
+            <Swatch name={color} hex={colors[color]} key={color}/>
+        ))}
+    </Artboard>
+);
+
+Document.propTypes = {
+    colors: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default () => {
+    const colorList = {
+        Haus: '#F3F4F4',
+        Night: '#333',
+        Sur: '#96DBE4',
+        'Sur Dark': '#24828F',
+        Peach: '#EFADA0',
+        'Peach Dark': '#E37059',
+        Pear: '#93DAAB',
+        'Pear Dark': '#2E854B',
+    };
+
+    render(<Document colors={colorList}/>, context.document.currentPage());
+};
