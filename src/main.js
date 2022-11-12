ac78a9e2aa32bb85f57a8648fe26be4c77557d1d
bye-sharif <sharif.chowdhury@bkash.com>
2022-11-12 09:22:17 +0600

    Add basic webview react

--- src/main.js
+++ src/main.js
@@ -1,31 +1,47 @@
-/* global context */
-import * as React from 'react';
-import {Document as RootDocument, Page, render} from 'react-sketchapp';
+import BrowserWindow from 'sketch-module-web-view'
+import { getWebview } from 'sketch-module-web-view/remote'
+import UI from 'sketch/ui'
 
-import App from './App';
+const webviewIdentifier = 'my-plugin.webview'
 
-const pages = [
-    {
-        name: 'App',
-        component: App,
-    },
-];
+export default function () {
+    const options = {
+        identifier: webviewIdentifier,
+        width: 240,
+        height: 180,
+        show: false
+    }
 
-const Document = () => (
-    <RootDocument>
-        {pages.map(({name, component: Component}) => (
-            <Page name={name}>
-                <Component/>
-            </Page>
-        ))}
-    </RootDocument>
-);
+    const browserWindow = new BrowserWindow(options)
 
-export default () => {
-    const data = context.document.documentData();
-    const pages = context.document.pages();
+    // only show the window when the page has loaded to avoid a white flash
+    browserWindow.once('ready-to-show', () => {
+        browserWindow.show()
+    })
 
-    data.setCurrentPage(pages.firstObject());
+    const webContents = browserWindow.webContents
 
-    render(<Document/>);
-};
+    // print a message when the page loads
+    webContents.on('did-finish-load', () => {
+        UI.message('UI loaded!')
+    })
+
+    // add a handler for a call from web content's javascript
+    webContents.on('nativeLog', s => {
+        UI.message(s)
+        webContents
+            .executeJavaScript(`setRandomNumber(${Math.random()})`)
+            .catch(console.error)
+    })
+
+    browserWindow.loadURL(require('../resources/webview.html'))
+}
+
+// When the plugin is shutdown by Sketch (for example when the user disable the plugin)
+// we need to close the webview if it's open
+export function onShutdown() {
+    const existingWebview = getWebview(webviewIdentifier)
+    if (existingWebview) {
+        existingWebview.close()
+    }
+}
